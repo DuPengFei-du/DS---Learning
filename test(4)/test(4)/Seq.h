4d0649b8d183dfbde594ea1fee64e4ae5d7112dc
DuPengFei-du <935820240@qq.com>
2020-05-18 13:05:28 +0800

    test

---
+++ test(4)/test(4)/Seq.h
@@ -0,0 +1,33 @@
+#pragma once 
+
+#define Default_Capacity 100
+
+#include<stdio.h>
+#include<assert.h>
+#include<string.h>
+#include<stdlib.h>
+
+typedef struct SeqList
+{
+	int* array;
+	int _size;
+	int _capacity;
+}SeqList;
+
+void SeqListInit(SeqList* seq);
+
+void SeqListDestroy(SeqList* seq);
+
+void SeqListPushBack(SeqList* seq,int data);
+
+void SeqListPushFront(SeqList* seq, int data);
+
+void SeqListPopFront(SeqList* seq);
+
+void SeqListPopBack(SeqList* seq);
+
+void SeqListPrint(SeqList* seq);
+
+void SeqListInsert(SeqList* seq,int index,int data);
+
+void SeqListErase(SeqList* seq, int index);
