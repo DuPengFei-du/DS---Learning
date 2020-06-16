6d54f4d4122101bd71a1c27b2a6ebc560a3b0484
DuPengFei-du <935820240@qq.com>
2020-06-16 08:02:07 +0800

    test

---
+++ DS(67)/DS(67)/main.c
@@ -0,0 +1,40 @@
+#include"List.h"
+
+int main()
+{
+	List s;
+
+	ListInit(&s);
+
+	ListPushBack(&s, 10);
+	ListPushBack(&s, 20);
+	ListPushBack(&s, 30);
+	ListPushBack(&s, 40);
+	ListPushBack(&s, 50);
+
+	ListPrint(&s);
+
+	ListPushFront(&s, 1);
+	ListPushFront(&s, 2);
+	ListPushFront(&s, 3);
+	ListPushFront(&s, 4);
+	ListPushFront(&s, 5);
+
+	ListPrint(&s);
+
+	ListPopFront(&s);
+	ListPopFront(&s);
+
+	ListPrint(&s);
+
+	ListPopBack(&s);
+	ListPopBack(&s);
+
+	ListPrint(&s);
+
+	ListInsert(ListFind(&s, 20), 46);
+
+	ListPrint(&s);
+
+	ListDestroy(&s);
+}
