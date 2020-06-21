5caf70720018ed683c94efd0fcf4f19615edd6d0
DuPengFei-du <935820240@qq.com>
2020-06-22 07:36:16 +0800

    test

---
+++ DS(69)/DS(69)/main.c
@@ -0,0 +1,47 @@
+#include"Seq.h"
+
+
+int main()
+{
+	SeqList s;
+
+	SeqListInit(&s);
+
+
+	SeqListPushBack(&s, 10);
+	SeqListPushBack(&s, 20);
+	SeqListPushBack(&s, 30);
+	SeqListPushBack(&s, 40);
+	SeqListPushBack(&s, 50);
+
+	SeqListPrint(&s);
+
+	SeqListPushFront(&s, 1);
+	SeqListPushFront(&s, 2);
+	SeqListPushFront(&s, 3);
+	SeqListPushFront(&s, 4);
+	SeqListPushFront(&s, 5);
+
+	SeqListPrint(&s);
+
+	SeqListPopFront(&s);
+	SeqListPopFront(&s);
+
+	SeqListPrint(&s);
+
+	SeqListPopBack(&s);
+	SeqListPopBack(&s);
+
+	SeqListPrint(&s);
+
+	SeqListInsert(&s, 2, 46);
+
+	SeqListPrint(&s);
+
+	SeqListErase(&s, 2);
+
+	SeqListPrint(&s);
+
+	SeqListDestroy(&s);
+
+}
