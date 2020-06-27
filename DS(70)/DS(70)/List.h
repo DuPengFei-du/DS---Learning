79c10bc59fdde644250b087ff935d829931dd15c
DuPengFei-du <935820240@qq.com>
2020-06-28 07:49:00 +0800

    test

---
+++ DS(70)/DS(70)/List.h
@@ -0,0 +1,36 @@
+#pragma once 
+
+#include<stdio.h>
+#include<assert.h>
+#include<stdlib.h>
+#include<string.h>
+
+
+typedef struct ListNode
+{
+	int data;
+	struct ListNode* next;
+}Node;
+
+
+typedef struct List
+{
+	Node* head;
+}List;
+
+
+void ListInit(List* pl);
+
+void ListDestroy(List* pl);
+
+void ListPushBack(List* pl,int data);
+
+void ListPushFront(List* pl, int data);
+
+void ListPopBack(List* pl);
+
+void ListPopFront(List* pl);
+
+void ListPrint(List* pl);
+
+Node* ListFind(List* pl, int data);
