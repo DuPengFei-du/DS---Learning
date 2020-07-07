ee8d4508d4c09562d2561689ec6f68790ed3745b
DuPengFei-du <935820240@qq.com>
2020-07-08 07:46:36 +0800

    test

---
+++ DS(75)/DS(75)/Seq.h
@@ -0,0 +1,37 @@
+#pragma once 
+
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<assert.h>
+#include<string.h>
+
+#define Default_Cpacity 100
+
+typedef struct SeqList
+{
+	int* array;
+	int _size;
+	int _capacity;
+}SeqList;
+
+
+void SeqListInit(SeqList* seq);
+
+void SeqListPrint(SeqList* seq);
+
+void SeqListDestroy(SeqList* seq);  
+
+void SeqListPushBack(SeqList* seq, int data);  
+
+void SeqListPopBack(SeqList* seq);  
+
+void SeqListPushFront(SeqList* seq, int data);
+
+void SeqListPopFront(SeqList* seq); 
+
+void SeqListInsert(SeqList* seq, int index, int data);
+
+void SeqListErase(SeqList* seq, int index); 
+
+int SeqListFind(SeqList* seq, int data);
---
+++ DS(75)/DS(75)/main.c
@@ -0,0 +1,38 @@
+#include"Seq.h"
+
+
+int main()
+{
+	SeqList s;
+
+	SeqListInit(&s);
+
+	SeqListPushBack(&s, 1);
+	SeqListPushBack(&s, 2);
+	SeqListPushBack(&s, 3);
+	SeqListPushBack(&s, 4);
+	SeqListPushBack(&s, 5);
+
+	SeqListPrint(&s);
+
+	SeqListPushFront(&s, 10);
+	SeqListPushFront(&s, 20);
+	SeqListPushFront(&s, 30);
+	SeqListPushFront(&s, 40);
+	SeqListPushFront(&s, 50);
+
+	SeqListPrint(&s);
+
+	SeqListPopFront(&s);
+	SeqListPopFront(&s);
+
+	SeqListPrint(&s);
+
+	SeqListPopBack(&s);
+	SeqListPopBack(&s);
+
+	SeqListPrint(&s);
+
+	SeqListDestroy(&s);
+
+}
