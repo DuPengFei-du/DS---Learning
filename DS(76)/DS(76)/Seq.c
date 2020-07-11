e7c94990c853c8a1f37a98c1b60e8e7072b6737c
DuPengFei-du <935820240@qq.com>
2020-07-11 08:04:26 +0800

    test

---
+++ DS(76)/DS(76)/Seq.c
@@ -0,0 +1,124 @@
+#include"Seq.h"
+
+
+void SeqListInit(SeqList* seq)
+{
+	assert(seq);
+	seq->array = (int*)malloc(sizeof(int) * Default_Capacity);
+	if (NULL == seq->array)
+	{
+		assert(0);
+		return;
+	}
+	seq->_size = 0;
+	seq->_capacity = Default_Capacity;
+}
+
+void SeqListDestroy(SeqList* seq)
+{
+	assert(seq);
+	free(seq->array);
+	seq->array = NULL;
+	seq->_capacity = 0;
+	seq->_size = 0;
+}
+
+void CheckCapacity(SeqList* seq)
+{
+	assert(seq);
+	if (seq->_size == seq->_capacity)
+	{
+		int newcapacity = 2 * Default_Capacity;
+		int* temp = (int*)malloc(sizeof(int)*newcapacity);
+		if (NULL == temp)
+		{
+			assert(0);
+			return;
+		}
+		memcpy(temp, seq->array, sizeof(int) * seq->_size);
+		free(seq->array);
+		seq->array = temp;
+		seq->_capacity = newcapacity;
+	}
+}
+
+void SeqListPushBack(SeqList* seq, int data)
+{
+	assert(seq);
+	CheckCapacity(seq);
+	seq->array[seq->_size] = data;
+	seq->_size++;
+}
+
+void SeqListPushFront(SeqList* seq, int data)
+{
+	assert(seq);
+	CheckCapacity(seq);
+	for (int i = seq->_size - 1; i >= 0; --i)
+	{
+		seq->array[i + 1] = seq->array[i];
+	}
+	seq->array[0] = data;
+	seq->_size++;
+}
+
+void SeqListPopFront(SeqList* seq)
+{
+	assert(seq);
+	for (int i = 0; i <= seq->_size - 2; i++)
+	{
+		seq->array[i] = seq->array[i + 1];
+	}
+	seq->_size--;
+}
+
+void SeqListPopBack(SeqList* seq)
+{
+	assert(seq);
+	if (seq->_size == 0)
+		return;
+	seq->_size--;
+}
+
+void SeqListPrint(SeqList* seq)
+{
+	assert(seq);
+	for (int i = 0; i < seq->_size; i++)
+	{
+		printf("%d--->", seq->array[i]);
+	}
+	printf("NULL\n");
+}
+
+void SeqListInsert(SeqList* seq, int index, int data)
+{
+	assert(seq);
+	CheckCapacity(seq);
+	for (int i = seq->_size - 1; i >= index; --i)
+	{
+		seq->array[i + 1] = seq->array[i];
+	}
+	seq->array[index] = data;
+	seq->_size++;
+}
+
+void SeqListErase(SeqList* seq, int index)
+{
+	assert(seq);
+	for (int i = index; i <= seq->_size - 2; i++)
+	{
+		seq->array[i] = seq->array[i + 1];
+	}
+	seq->_size--;
+}
+
+int SeqListFind(SeqList* seq, int data)
+{
+	assert(seq);
+	for (int i = 0; i < seq->_size; i++)
+	{
+		if (seq->array[i] == data)
+			return i;
+	}
+	return -1;
+}
