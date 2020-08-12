18033fd2f168fabc64fb7a679e6f998bc2be1eb8
DuPengFei-du <935820240@qq.com>
2020-08-12 08:07:24 +0800

    test

---
+++ DS(84)/DS(84)/main.c
@@ -0,0 +1,141 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	int arr[10] = { 1,3,5,7,9,2,4,6,8,10 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int i = 0;
+//	for (i = 0; i < sz - 1; i++)
+//	{
+//		for (int j = 0; j < sz - i - 1; j++)
+//		{
+//			if (arr[j] > arr[j + 1])
+//			{
+//				int temp = arr[j];
+//				arr[j] = arr[j + 1];
+//				arr[j + 1] = temp;
+//			}
+//		}
+//	}
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	int arr[10] = { 1,3,5,7,9,2,4,6,8,10 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int i = 0;
+//	for (i = 0; i < sz - 1; i++)
+//	{
+//		int flag = 1;
+//		for (int j = 0; j < sz - i - 1; j++)
+//		{
+//			if (arr[j] > arr[j + 1])
+//			{
+//				int temp = arr[j];
+//				arr[j] = arr[j + 1];
+//				arr[j + 1] = temp;
+//				flag = 0;
+//			}
+//		}
+//		if (flag == 1)
+//			break;
+//	}
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int left = 0;
+//	int right = sz - 1;
+//	int key = 8;
+//	while (left <= right)
+//	{
+//		int mid = left + (right - left) / 2;
+//		if (arr[mid] < key)
+//			left = mid + 1;
+//		else if (arr[mid] > key)
+//			right = mid - 1;
+//		else
+//		{
+//			printf("找到了，下标为:%d", mid);
+//			break;
+//		}
+//	}
+//	return -1;
+//}
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	int i = 1;
+//	int sum = 1;
+//	int n = 0;
+//	(void)scanf("%d", &n);
+//	for (i = 1; i <= n; i++)
+//	{
+//		sum *= i;
+//	}
+//	printf("%d", sum);
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//long long fib(int n)
+//{
+//	long long a = 1;
+//	long long b = 1;
+//	long long c = 1;
+//	for (int i = 3; i <= n; i++)
+//	{
+//		c = a + b;
+//		a = b;
+//		b = c;
+//	}
+//	return b;
+//}
+//int main()
+//{
+//	int  n = 0;
+//	(void)scanf("%d", &n);
+//	printf("%lld", fib(n));
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//long long fib(long long a, long long b,int n)
+//{
+//	if (n == 3)
+//		return a + b;
+//	else if (n < 3)
+//		return 1;
+//	else
+//		return fib(b, a + b, n - 1);
+//}
+//int main()
+//{
+//	int  n = 0;
+//	(void)scanf("%d", &n);
+//	printf("%lld", fib(1,1,n));
+//	return 0;
+//}
