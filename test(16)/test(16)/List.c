89f787d3d13e181a4a0615d39fbf45348eba6187
DuPengFei-du <935820240@qq.com>
2020-09-21 08:03:20 +0800

    test

---
+++ test(16)/test(16)/List.c
@@ -0,0 +1,136 @@
+#include"List.h"
+
+
+void ListInit(List* pl)
+{
+	assert(pl);
+	pl->head = NULL;
+}
+
+void ListDestroy(List* pl)
+{
+	Node* cur = NULL;
+	assert(pl);
+	cur = pl->head;
+	while (cur)
+	{
+		pl->head = cur->next;
+		free(cur);
+		cur = pl->head;
+	}
+	pl->head = NULL;
+}
+
+Node* CreateNode(int data)
+{
+	Node* pNewNode = (Node*)malloc(sizeof(Node));
+	if (NULL == pNewNode)
+	{
+		assert(0);
+		return NULL;
+	}
+	pNewNode->data = data;
+	pNewNode->next = NULL;
+	return pNewNode;
+}
+
+void ListPushBack(List* pl, int data)
+{
+	Node* cur = NULL;
+	Node* pNewNode = NULL;
+	assert(pl);
+	pNewNode = CreateNode(data);
+	if (pl->head == NULL)
+	{
+		pl->head = pNewNode;
+	}
+	else
+	{
+		cur = pl->head;
+		while (cur->next)
+		{
+			cur = cur->next;
+		}
+		cur->next = pNewNode;
+		pNewNode->next = NULL;
+	}
+}
+
+void ListPushFront(List* pl, int data)
+{
+	Node* pNewNode = NULL;
+	assert(pl);
+	pNewNode = CreateNode(data);
+	pNewNode->next = pl->head;
+	pl->head= pNewNode;
+}
+
+void ListPopFront(List* pl)
+{
+	Node* cur = NULL;
+	assert(pl);
+	cur = pl->head;
+	pl->head = cur->next;
+	free(cur);
+	cur = pl->head;
+}
+
+void ListPopBack(List* pl)
+{
+	Node* cur = NULL;
+	assert(pl);
+	cur = pl->head;
+	if (NULL == pl->head)
+		return;
+	else if (NULL == pl->head->next)
+	{
+		free(pl->head);
+		pl->head = NULL;
+	}
+	else
+	{
+		while (cur->next->next)
+		{
+			cur = cur->next;
+		}
+		free(cur->next);
+		cur->next = NULL;
+	}
+}
+
+void ListPrint(List* pl)
+{
+	Node* cur = NULL;
+	assert(pl);
+	cur = pl->head;
+	while (cur)
+	{
+		printf("%d--->", cur->data);
+		cur = cur->next;
+	}
+	printf("NULL\n");
+}
+
+void ListInsert(Node* pos, int data)
+{
+	Node* pNewNode = NULL;
+	if (NULL == pos)
+		return;
+	pNewNode = CreateNode(data);
+	pNewNode->next = pos->next;
+	pos->next = pNewNode;
+}
+
+Node* ListFind(List* pl, int data)
+{
+	Node* cur = NULL;
+	assert(pl);
+	cur = pl->head;
+	while (cur)
+	{
+		if (cur->data == data)
+			return cur;
+		cur = cur->next;
+	}
+	return NULL;
+}
