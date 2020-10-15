c855aafcc33398524beed6de43dc384b1ba2b45f
DuPengFei-du <935820240@qq.com>
2020-10-15 10:10:19 +0800

    test

---
+++ Prac(3)/Prac(3)/List.h
@@ -0,0 +1,40 @@
+#pragma once
+
+
+
+#include<stdio.h>
+#include<assert.h>
+#include<string.h>
+#include<stdlib.h>
+
+
+typedef struct ListNode
+{
+	int data;
+	struct ListNode* next;
+}Node;
+
+
+typedef struct List
+{
+	Node* head;
+}List;
+
+
+void ListInit(List* pl);
+
+void ListDestroy(List* pl);
+
+void ListPushBack(List* pl,int data);
+
+void ListPushFront(List* pl, int data);
+
+void ListPopFront(List* pl);
+
+void ListPopBack(List* pl);
+
+void ListPrint(List* pl);
+
+void ListInsert(Node *pos,int data);
+
+Node *ListFind(List* pl, int data);
