383cc9dc7361440de0c0b86d89f4c15d416b057d
DuPengFei-du <935820240@qq.com>
2020-12-15 10:16:28 +0800

    test

---
+++ test(26)/test(26)/Stack.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include<stdio.h>
+#include<assert.h>
+#include<string.h>
+#include<stdlib.h>
+
+
+#define Default_Capacity 100
+
+
+typedef struct Stack
+{
+	int* array;
+	int _top;
+	int _capacity;
+}Stack;
+
+void StackInit(Stack* s);
+
+void StackDestroy(Stack* s);
+
+void StackPush(Stack* s,int data);
+
+void StackPop(Stack* s);
+
+int StackEmpty(Stack* s);
+
+int StackTop(Stack* s); 
+
+int StackSize(Stack* s);
