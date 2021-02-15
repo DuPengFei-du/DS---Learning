cf126756e5da4c05e09454aade1aa66cbc434d7c
DuPengFei-du <935820240@qq.com>
2021-02-15 23:11:13 +0800

    test

---
+++ Prac(44)/Prac(44)/main.c
@@ -0,0 +1,26 @@
+#include"Queue.h"
+
+
+int main()
+{
+	Queue q;
+
+	QueueInit(&q);
+
+	QueuePush(&q, 10);
+	QueuePush(&q, 20);
+	QueuePush(&q, 30);
+	QueuePush(&q, 40);
+	QueuePush(&q, 50);
+
+	printf("%d\n", QueueSize(&q));
+	printf("%d\n", QueueEmpty(&q));
+
+	QueuePop(&q);
+	QueuePop(&q);
+
+
+	printf("%d\n", QueueSize(&q));
+	printf("%d\n", QueueEmpty(&q));
+	QueueDestroy(&q);
+}
