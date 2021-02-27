707cd1cd88cd06efb63347ffe10dbac81e01f379
DuPengFei-du <935820240@qq.com>
2021-02-27 20:06:07 +0800

    test

---
+++ test(38)/test(38)/Stack.c
@@ -0,0 +1,79 @@
+#include"Stack.h"
+
+
+void StackInit(Stack* s)
+{
+	assert(s);
+	s->array = (int*)malloc(sizeof(int) * Default_Capcity);
+	if (NULL == s->array)
+	{
+		assert(0);
+		return;
+	}
+	s->_capacity = Default_Capcity;
+	s->_top = 0;
+}
+
+void StackDestroy(Stack* s)
+{
+	assert(s);
+	free(s->array);
+	s->array = NULL;
+	s->_top = 0;
+	s->_capacity = 0;
+}
+
+void CheckCapacity(Stack* s)
+{
+	assert(s);
+	if (s->_capacity == s->_top)
+	{
+		int newcapacity = 2 * Default_Capcity;
+		int* temp = (int*)malloc(sizeof(int) * newcapacity);
+		if (NULL == temp)
+		{
+			assert(0);
+			return;
+		}
+		memcpy(temp, s->array, sizeof(int) * s->_top);
+		free(s->array);
+		s->array = temp;
+		s->_capacity = newcapacity;
+	}
+}
+
+void StackPush(Stack* s, int data)
+{
+	assert(s);
+	CheckCapacity(s);
+	s->array[s->_top ] = data;
+	s->_top++;
+}
+
+void StackPop(Stack* s)
+{
+	assert(s);
+	if (0 == s->_top)
+		return;
+	s->_top--;
+}
+
+int StackEmpty(Stack* s)
+{
+	assert(s);
+	if (s->_top == 0)
+		return 1;
+	return 0;
+}
+
+int StackTop(Stack* s)
+{
+	assert(s);
+	return s->array[s->_top - 1];
+}
+
+int StackSize(Stack* s)
+{
+	assert(s);
+	return s->_top;
+}
---
+++ test(38)/test(38)/Stack.h
@@ -0,0 +1,32 @@
+#pragma once
+
+
+#include<stdio.h>
+#include<assert.h>
+#include<stdlib.h>
+#include<string.h>
+
+#define Default_Capcity 100
+
+
+typedef struct Stack
+{
+	int* array;
+	int _top;
+	int _capacity;
+}Stack;
+
+
+void StackInit(Stack* s);
+
+void StackDestroy(Stack* s);
+
+void StackPush(Stack* s,int data);
+
+void StackPop(Stack* s);
+
+int StackEmpty(Stack* s);
+
+int StackTop(Stack* s);
+
+int StackSize(Stack* s);
