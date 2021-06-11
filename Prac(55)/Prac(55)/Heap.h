60e01aeb921e8b314936f9095839da2ceef1fea4
DuPengFei-du <935820240@qq.com>
2021-06-11 23:08:14 +0800

    test

---
+++ Prac(55)/Prac(55)/Heap.h
@@ -0,0 +1,42 @@
+#pragma once 
+
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<assert.h>
+#include<string.h>
+
+
+typedef int HPDataType;
+
+
+typedef struct Heap
+{
+	HPDataType* array;
+	int _size;
+	int _capacity;
+}Heap;
+
+
+
+void HeapInit(Heap* hp,int init);
+
+void HeapCreate(Heap* hp, HPDataType *array,int size);
+
+void HeapDestroy(Heap* hp);
+
+void HeapPop(Heap* hp);
+
+void HeapPush(Heap* hp, HPDataType data);
+
+HPDataType HeapTop(Heap* hp);
+
+int HeapSize(Heap* hp);
+
+int HeapEmpty(Heap* hp);
+
+
+
+
+/////////////////////////////////
+void TestHeap();
