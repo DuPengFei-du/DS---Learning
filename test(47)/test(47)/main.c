e628c938e8326028549bdc6b98c21ce3db9b5202
DuPengFei-du <935820240@qq.com>
2021-07-30 19:51:27 +0800

    test

---
+++ test(47)/test(47)/main.c
@@ -0,0 +1,41 @@
+#include"List.h"
+
+
+int main()
+{
+	List s;
+
+	NodeInit(&s);
+
+	NodePushBack(&s, 10);
+	NodePushBack(&s, 20);
+	NodePushBack(&s, 30);
+	NodePushBack(&s, 40);
+	NodePushBack(&s, 50);
+
+	NodePrint(&s);
+
+	NodePushFront(&s, 1);
+	NodePushFront(&s, 2);
+	NodePushFront(&s, 3);
+	NodePushFront(&s, 4);
+	NodePushFront(&s, 5);
+
+	NodePrint(&s);
+
+	NodePopBack(&s);
+	NodePopBack(&s);
+
+	NodePrint(&s);
+
+	NodePopFront(&s);
+	NodePopFront(&s);
+
+	NodePrint(&s);
+
+	NodeInsert(NodeFind(&s, 30), 46);
+
+	NodePrint(&s);
+
+	NodeDestroy(&s);
+}
